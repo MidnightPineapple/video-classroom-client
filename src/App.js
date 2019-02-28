a873f128148a054fadf7150aab8d09e1a587acfd
MidnightPineapple <samuel88835@yahoo.com>
2019-02-28 08:32:55 -0500

    before eject

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ipcRenderer } from 'electron'; 
 
 class App extends Component {
   render() {
+    ipcRenderer.send("ready")
+
     return (
       <div className="App">
         <header className="App-header">

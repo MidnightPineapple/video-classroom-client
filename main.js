169b3ea6773616e11a0701d1c671bf279131aeaa
MidnightPineapple <samuel88835@yahoo.com>
2019-03-01 12:20:18 -0500

    hide dev tools

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 function createWindow() {
     mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadFile(path.join("build", "index.html"));
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
     mainWindow.on('closed', function () {
         mainWindow = null
     })

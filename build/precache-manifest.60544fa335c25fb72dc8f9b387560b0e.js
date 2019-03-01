40e861627e1a506992fc9a0a29c4ae190209bd63
MidnightPineapple <samuel88835@yahoo.com>
2019-03-01 10:38:57 -0500

    include build so master is production branch

---
+++ build/precache-manifest.60544fa335c25fb72dc8f9b387560b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "904c3c78e722eb60593f",
+    "url": "./static/css/main.41fd8ff3.chunk.css"
+  },
+  {
+    "revision": "904c3c78e722eb60593f",
+    "url": "./static/js/main.904c3c78.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "4e347b60224ec2e09924",
+    "url": "./static/js/2.4e347b60.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3aec487707ba6295b4aa2643b7e2eab2",
+    "url": "./index.html"
+  }
+];
